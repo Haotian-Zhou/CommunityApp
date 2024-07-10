bffeab7d8183d09cc066450bec67ea7e52240f1a
John Haotian Zhou <haotianz0422@gmail.com>
2024-07-10 19:12:36 -0400

    upload01

--- src/main/java/ca/bytetube/communityApp/CommunityappApplication.java
+++ src/main/java/ca/bytetube/communityApp/CommunityappApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CommunityAppSpringbootApplication {
+public class CommunityappApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CommunityAppSpringbootApplication.class, args);
+        SpringApplication.run(CommunityappApplication.class, args);
     }
 
 }
--- src/main/java/ca/bytetube/communityApp/Hello.java
+++ src/main/java/ca/bytetube/communityApp/Hello.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hello {
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello() {
-        //返回Hello SpringBoot!
-        return "Hello SpringBoot!";
-    }
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String hello() {
+		//返回Hello SpringBoot!
+		return "Hello SpringBoot!";
+	}
 }
